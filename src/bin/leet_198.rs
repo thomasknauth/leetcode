0987ae068a69a62cedaff0d2c57d2641abe25741
Thomas Knauth <thomas.knauth@gmx.de>
2025-11-05 09:40:58 +0100

    chore: formatting

--- src/bin/leet_198.rs
+++ src/bin/leet_198.rs
@@ -1,6 +1,17 @@
+// 198 House Robber
+//
+// You are a professional robber planning to rob houses along a street. Each house has a certain
+// amount of money stashed, the only constraint stopping you from robbing each of them is that
+// adjacent houses have security systems connected and it will automatically contact the police if
+// two adjacent houses were broken into on the same night.
+//
+// Given an integer array nums representing the amount of money of each house, return the maximum
+// amount of money you can rob tonight without alerting the police.
+//
 // Find max sum, w/o adding two adjacent elements.
-
-// [0, 1, 0] -> maximizing # of added elements is not the goal, in this example, we just want the 2nd element for the max sum
+//
+// [0, 1, 0] -> maximizing # of added elements is not the goal, in this example, we just want the
+// 2nd element for the max sum
 
 struct Solution {}
 
